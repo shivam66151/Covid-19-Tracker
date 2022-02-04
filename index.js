dbe59dea35a337b00e518fde76d9a389d89801bd
Shivam Sharma <sharmashivam66151@gmail.com>
2022-02-04 18:12:34 +0000

    1 file commited

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 let btn = document.getElementById("btn");
+
+btn.addEventListener("click" ,() => {
+    let text = document.getElementById("getText").value;
+
+    fetch('https://api.covid19api.com/summary')
+    .then((covidData) => {
+        return covidData.json();
+    })
+    .then((getData) => {
+        console.log(getData);
+        var content = document.querySelector(".data");
+
+
+        let data = document.querySelector(".data");
+        data.innerHTML = `<div class="box">
+                        <div class="head">
+                          <span> COVID-19 Cases in ${getData.Countries[index].Country} </span>
+                        </div>
+                        <div class="total">
+                            <div><p>TotalConfirmed</p> ${getData.Countries[index].TotalConfirmed}</div>
+                            <div><p>TotalDeaths</p> ${getData.Countries[index].TotalDeaths}</div>
+                            <div><p>TotalRecovered</p> ${getData.Countries[index].TotalRecovered}</div>
+                        </div>
+                        <div class="new">
+                            <div><p>NewConfirmed</p> ${getData.Countries[index].NewConfirmed}</div>
+                            <div><p>NewDeaths</p> ${getData.Countries[index].NewDeaths}</div>
+                            <div><p>NewRecovered</p> ${getData.Countries[index].NewRecovered}</div>
+                        </div>
+                           </div>`;
+        
+    })
+})
